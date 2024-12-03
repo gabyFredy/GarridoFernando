7783c69984043a03365834ea16b224130f2ffe52
gabyFredy <20233TN189@utez.edu.mx>
2024-12-03 12:30:55 -0600

    Add files via upload

---
+++ UV_E4_GarridoLucena_FernandoGarrido/UV_E4_Apellidos_Nombres.java
@@ -0,0 +1,45 @@
+/** Generar un arbol con la siguiente estructura y valores:
+ *                 100 
+ *             /        \
+ *           34          56
+ *          /  \       /    \
+ *        23   38     43     79
+ *       /  \    \   /  \   /  \
+ *      12  24   41 42  44 55  95
+ *
+ * 
+ * Luego desarrolla 3 algoritmos que al pasarles el árbol, lo 
+ * voltee dejando el siguiente:
+ * 
+ *                   100
+ *                 /      \
+ *               56        34
+ *              /  \      /    \
+ *            79    43   38    23
+ *           /  \  /  \    \   /  \
+ *          95  55 44 42   41  24  12
+ * 
+ * Observa que los valores de la derecha se pasaron a la izquierda
+ * 
+ * El primer algoritmo debe manejar una Pila (la que hicimos en clase),
+ * El segundo, una Cola (la que hicimos en clase)
+ * Y finalmente el ultimo debe ser recursivo sin manejar nada más 
+ * que la raíz del árbol
+ * 
+ * Rellenar: Nombres de los integrantes del equipo:
+ *   1)
+ *   2)
+ *   3)
+ *   4)
+ *   5)
+ *   6)
+ * 
+ **/
+public class UV_E4_Apellidos_Nombres {
+    public static void main (String[] args){
+        //Prueba tu código aquí
+    }
+
+    //Programa tus métodos aqui
+
+}
